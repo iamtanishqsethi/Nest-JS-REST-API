885819d5193fbc17cbdd1341ff25fb11c3a0a20d
Tanishq Sethi <tanishqsethi05@gmail.com>
2025-11-30 13:35:23 +0530

    feat: add throttling support with @nestjs/throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,28 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { EmployeesModule } from './employees/employees.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, EmployeesModule],
+  imports: [
+    UsersModule,
+    DatabaseModule,
+    EmployeesModule,
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
+  ], // The above line indicates no more than 3 requests in 1 Sec and no more than 100 requests in 60 sec
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -10,7 +10,9 @@ import {
 } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { Prisma } from '@prisma/client';
+import { Throttle, SkipThrottle } from '@nestjs/throttler';
 
+@SkipThrottle() // this will make sure it skips Throttling for all the routes
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
@@ -20,11 +22,13 @@ export class EmployeesController {
     return this.employeesService.create(createEmployeeDto);
   }
 
+  @SkipThrottle({ default: false }) // this will rate limit this one request
   @Get()
   findAll(@Query('role') role?: 'intern' | 'engineer' | 'admin') {
     return this.employeesService.findAll(role);
   }
 
+  @Throttle({ short: { ttl: 1000, limit: 1 } }) // named rate limiting and overriding that
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.employeesService.findOne(+id);
